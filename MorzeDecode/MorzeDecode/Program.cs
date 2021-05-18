4e15713f40c5c2b02de8aaaef31b160e994708bc
qdenka <a@bigf.at>
2021-05-18 23:27:10 +0300

    Декодирование азбуки морзе

---
+++ MorzeDecode/MorzeDecode/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MorzeDecode
+{
+    class Program
+    {
+        public static IDictionary<string, string> MorseCodeDictionary = new Dictionary<string, string>() {
+             { ".-", "A"},
+             { "-...", "B"},
+             { "-.-.", "C"},
+             { "-..", "D"},
+             { ".", "E"},
+             { "..-.", "F"},
+             { "--.", "G"},
+             { "....", "H"},
+             { "..", "I"},
+             { ".---", "J"},
+             { "-.-", "K"},
+             { ".-..", "L"},
+             { "--", "M"},
+             { "-.", "N"},
+             { "---", "O"},
+             { ".--.", "P"},
+             { "--.-", "Q"},
+             { ".-.", "R"},
+             { "...", "S"},
+             { "-", "T"},
+             { "..-", "U"},
+             { "...-", "V"},
+             { ".--", "W"},
+             { "-..-", "X"},
+             { "-.--", "Y"},
+             { "--..", "Z"},
+        };
+
+        static void Main(string[] args)
+        {
+            string morzeText = WriteMessage();
+
+            while(morzeText != "stop")
+            {
+                Console.WriteLine("\n{0} - декодированная строка\n", Decode(morzeText));
+
+                morzeText = WriteMessage();
+            }
+            Console.WriteLine("Программа окончила работу");
+        }
+
+        static string WriteMessage()
+        {
+            Console.WriteLine("Введите текст для декодирования азбуки Морзе, чтобы окончить ввод введите stop:");
+            return Console.ReadLine();
+        }
+
+        static string Decode(string morzeText)
+        {
+            string decodedText = "";
+            string[] morzeString = morzeText.Split(" ");
+            foreach (string symbol in morzeString)
+            {
+                if (symbol == "") {
+                    decodedText += " ";
+                    continue;
+                }
+                if (MorseCodeDictionary.ContainsKey(symbol))
+                    decodedText += MorseCodeDictionary[symbol];
+                else
+                    decodedText += "*";
+            }
+            return decodedText;
+        }
+    }
+}
