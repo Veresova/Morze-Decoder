c8a9d311e9c48952af34e9ff581af15a6e940f48
qdenka <a@bigf.at>
2021-05-19 00:20:01 +0300

    Добавление автотестов

--- MorzeDecode/MorzeDecode/Program.cs
+++ MorzeDecode/MorzeDecode/Program.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 
 namespace MorzeDecode
 {
-    class Program
+    public class Program
     {
+        /**
+         * Объявление словаря азбуки морзе
+         */
         public static IDictionary<string, string> MorseCodeDictionary = new Dictionary<string, string>() {
              { ".-", "A"},
              { "-...", "B"},
@@ -34,41 +38,69 @@ namespace MorzeDecode
              { "--..", "Z"},
         };
 
-        static void Main(string[] args)
+        static void Main()
         {
-            string morzeText = WriteMessage();
+            // Вызываем функцию ввода строки
+            string morzeText = WriteWord();
 
-            while(morzeText != "stop")
+            // Цикл до тех пор пока строка не равна stop
+            while (morzeText != "stop")
             {
                 Console.WriteLine("\n{0} - декодированная строка\n", Decode(morzeText));
 
-                morzeText = WriteMessage();
+                morzeText = WriteWord();
             }
             Console.WriteLine("Программа окончила работу");
         }
 
-        static string WriteMessage()
+        /**
+         * Вывод сообщения с просьбой ввода строки, возврат введенной строки
+         * return string
+         */
+        static string WriteWord()
         {
             Console.WriteLine("Введите текст для декодирования азбуки Морзе, чтобы окончить ввод введите stop:");
+
             return Console.ReadLine();
         }
 
-        static string Decode(string morzeText)
+        /**
+         * Декодирование полученной в функции WriteMessage() строки
+         * string morzeText
+         * return string decodedText
+         */
+        public static string Decode(string morzeText)
         {
             string decodedText = "";
+            // Разделяем строку по пробелам
             string[] morzeString = morzeText.Split(" ");
             foreach (string symbol in morzeString)
             {
+                // Если было введено 2 пробела - значит новое слово
                 if (symbol == "") {
                     decodedText += " ";
                     continue;
                 }
+
                 if (MorseCodeDictionary.ContainsKey(symbol))
-                    decodedText += MorseCodeDictionary[symbol];
+                    decodedText += MorseCodeDictionary[symbol]; // Если нашлось вхождение в словарь, то добавляем букву
                 else
-                    decodedText += "*";
+                    decodedText += "*"; // Если не нашлось, то ставим звездочку на место буквы
             }
             return decodedText;
+        }
+    }
+
+    [TestFixture]
+    public class AutoTest
+    {
+        [Test]
+        public void MorzeTest()
+        {
+            string[] morzeArray = { ".- -... -.-.  .---- ..--- ...--", ".... . .-.. .-.. ---  .-- --- .-. .-.. -.." };
+            foreach (string morzeText in morzeArray) {
+                Console.WriteLine("\n{0} - декодированная строка\n", Program.Decode(morzeText));
+            }
         }
     }
 }
